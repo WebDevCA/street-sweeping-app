bf5f9cbe0a3bef83f61415aa702bc37c83765d3a
WebDevCA <christianz@netscape.net>
2026-03-25 23:29:30 -0700

    Add path module to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const db = require('./database');
 const { startScheduler, initializeWebPush } = require('./notificationScheduler');
 
